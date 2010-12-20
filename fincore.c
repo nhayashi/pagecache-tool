0ebe412dccab6d18b95dd49bccad8d98464de221
nhayashi <naritoshi.hayashi@gmail.com>
2010-12-20 14:35:42 +0900

    support 32bit arch

--- fincore.c
+++ fincore.c
@@ -1,23 +1,30 @@
-#include <errno.h> /* errno */
-#include <fcntl.h> /* fcntl, open */
-#include <stdio.h> /* perror, fprintf, stderr, printf */
-#include <stdlib.h> /* exit, calloc, free */
-#include <string.h> /* strerror */
-#include <sys/stat.h> /* stat, fstat */
-#include <sys/types.h> /* size_t */
-#include <unistd.h> /* sysconf, close */
-#include <sys/mman.h> /* mmap */
+#define _LARGEFILE_SOURCE
+#define _FILE_OFFSET_BITS 64
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/mman.h>
 
 int main(int argc, char *argv[]) {
     int fd;
+    off_t offset = 0, off_limit;
     struct stat sb;
     void *pa = (char *)0;
     unsigned char *vec = (unsigned char *)0;
-    size_t pageSize = sysconf(_SC_PAGE_SIZE);
-    register size_t pageIndex;
+    size_t page_size = sysconf(_SC_PAGE_SIZE);
+    register size_t page_index;
     int sum = 0;
 
-    printf("pagesize: %ld\n", pageSize);
+#ifdef __x86_64
+    printf("pagesize: %ld\n", page_size);
+#else
+    printf("pagesize: %d\n", page_size);
+#endif
 
     fd = open(argv[1], O_RDONLY);
     if (fd < 0) {
@@ -31,46 +38,65 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
+#ifdef __x86_64
     printf("filesize: %ld\n", sb.st_size);
+    off_limit = sb.st_size;
+    printf("offset limit: %ld\n", off_limit);
+#else
+    printf("filesize: %lld\n", sb.st_size);
+    off_limit = ((1024 * 1024 * 1024) / page_size) * page_size;
+    printf("offset limit: %lld\n", off_limit);
+#endif
 
-    pa = mmap((void *)0, sb.st_size, PROT_NONE, MAP_SHARED, fd, 0);
-    if (pa == MAP_FAILED) {
-        perror("mmap");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("page num: %ld\n", (sb.st_size + pageSize - 1) / pageSize);
-
-    vec = calloc(1, (sb.st_size + pageSize - 1) / pageSize);
-    if (vec == (void *)0) {
-        perror("calloc");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-
-    if (mincore(pa, sb.st_size, vec) != 0) {
-        fprintf(stderr, "mincore(%p, %lu, %p): %s\n",
-                pa, (unsigned long)sb.st_size, vec, strerror(errno));
-        free(vec);
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-
-    for (pageIndex = 0; pageIndex <= sb.st_size / pageSize; pageIndex++) {
-        if (vec[pageIndex] & 1) {
-            //printf("%lu\n", (unsigned long)pageIndex);
-            sum++;
+    while (sb.st_size > offset) {
+#ifdef __x86_64
+        printf("offset: %lu\n", offset);
+#else
+        printf("offset: %llu\n", offset);
+#endif
+        pa = mmap((void *)0, off_limit, PROT_NONE, MAP_SHARED, fd, offset);
+        if (pa == MAP_FAILED) {
+            perror("mmap");
+            close(fd);
+            exit(EXIT_FAILURE);
         }
+
+        printf("page num: %llu\n",
+                (unsigned long long)(off_limit + page_size - 1) / page_size);
+
+        vec = calloc(1, (off_limit + page_size - 1) / page_size);
+        if (vec == (void *)0) {
+            perror("calloc");
+            close(fd);
+            exit(EXIT_FAILURE);
+        }
+
+        if (mincore(pa, off_limit, vec) != 0) {
+            fprintf(stderr, "mincore(%p, %llu, %p): %s\n",
+                    pa, (unsigned long long)off_limit, vec, strerror(errno));
+            free(vec);
+            close(fd);
+            exit(EXIT_FAILURE);
+        }
+
+        for (page_index = 0; page_index <= off_limit / page_size; page_index++) {
+            if (vec[page_index] & 1) {
+                //printf("%lu\n", (unsigned long)page_index);
+                sum++;
+            }
+        }
+
+        free(vec);
+        vec = (unsigned char *)0;
+
+        munmap(pa, off_limit);
+
+        offset += off_limit;
     }
-
-    free(vec);
-    vec = (unsigned char *)0;
-
-    munmap(pa, sb.st_size);
     close(fd);
 
     printf("in memory pages: %d\n", sum);
+    printf("in memory size: %llu\n", (unsigned long long)sum * page_size);
 
     exit(EXIT_SUCCESS);
 }
